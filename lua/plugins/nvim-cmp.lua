fa1ea0814ac9934425eb59efea456cf4ff8aa059
twikou <eu.hi3107@gmail.com>
2025-05-08 16:57:35 +0700

    (refactor) break cmp config to multiple functions

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,3 +1,59 @@
+local sources = function(cmp)
+	return cmp.config.sources({
+		{ name = "luasnip" }
+	}, {
+		{ name = "buffer" },
+		{ name = "path" }
+	})
+end
+
+local mapping = function(cmp)
+	return cmp.mapping.preset.insert({})
+end
+
+local snippet = function(luasnip)
+	return {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end
+	}
+end
+
+local formatting = function(lspkind)
+	return {
+		format = lspkind.cmp_format({})
+	}
+end
+
+local setup = function(cmp, luasnip, lspkind)
+	cmp.setup({
+		snippet = snippet(luasnip),
+		mapping = mapping(cmp),
+		sources = sources(cmp),
+		formatting = formatting(lspkind),
+	})
+end
+
+local setup_cmdline = function(cmp)
+	cmp.setup.cmdline(':', {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources = cmp.config.sources({
+			{ name = 'path' }
+		}, {
+			{ name = 'cmdline' }
+		}),
+		matching = { disallow_symbol_nonprefix_matching = false }
+	})
+end
+
+local config = function()
+	local cmp = require("cmp")
+	local luasnip = require("luasnip")
+	local lspkind = require("lspkind")
+	setup(cmp, luasnip, lspkind)
+	setup_cmdline(cmp)
+end
+
 return {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
@@ -5,41 +61,10 @@ return {
 		"saadparwaiz1/cmp_luasnip",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline"
+		"hrsh7th/cmp-cmdline",
+		"onsails/lspkind.nvim"
 	},
 	event = "InsertEnter",
 	name = "cmp",
-	config = function()
-		local cmp = require("cmp")
-		local snippet = {
-			expand = function(args)
-				require('luasnip').lsp_expand(args.body)
-			end
-		}
-		local mapping = cmp.mapping.preset.insert({})
-		local sources = cmp.config.sources({
-			{ name = "luasnip" }
-		}, {
-			{ name = "buffer" },
-			{ name = "path" }
-		})
-		cmp.setup({
-			snippet = snippet,
-			mapping = mapping,
-			sources = sources,
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			},
-		})
-		cmp.setup.cmdline(':', {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = cmp.config.sources({
-				{ name = 'path' }
-			}, {
-				{ name = 'cmdline' }
-			}),
-			matching = { disallow_symbol_nonprefix_matching = false }
-		})
-	end
+	config = config,
 }

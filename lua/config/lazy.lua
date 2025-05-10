7dbd1869f0f20869ef77b70b24e1c2b25aefbaf8
twikou <eu.hi3107@gmail.com>
2025-05-10 13:40:19 +0700

    (fix) change map leader to \

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,7 +13,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	end
 end
 vim.opt.rtp:prepend(lazypath)
-vim.g.mapleader = " "
+vim.g.mapleader = "\\"
 vim.g.maplocalleader = "\\"
 require("lazy").setup({
 	spec = {
